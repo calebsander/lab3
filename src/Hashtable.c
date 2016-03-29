51dad14269f8399b852f8d8092c171ee59e9c92a
Alex Lew <alexl.mail@gmail.com>
2016-03-28 21:54:28 -0400

    Remove hashFunction.c, paste code into Hashtable.c

--- src/Hashtable.c
+++ src/Hashtable.c
@@ -1,3 +1,15 @@
 // Hashtable.c
 // Implements a hashtable for use in the Nine project.
 // (Your name and date here, as per style guide)
+
+// You may use this hash function in your implementation of a Hashtable.
+// Returns a hashed value for the given string in [0, size)
+static int hash(char *s, long size) {
+    unsigned long sum;
+    const long prime = 3141592661;
+    for (sum = 0; *s; s++) {
+        sum = (sum << 4) ^ *s;
+    }
+    return prime * sum % size;
+}
+
